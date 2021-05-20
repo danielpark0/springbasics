12ed60075e2bc77b1a36ba9a5ff90f4bbbf16a48
danielpark0 <37523983+danielpark0@users.noreply.github.com>
2021-05-20 19:44:25 +0900

    orderService

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -11,7 +11,7 @@ public class MemberApp {
         MemberService memberService = new MemberServiceImpl();
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
-        
+
         Member findMember = memberService.findMember(1L);
         System.out.println("new member = " + member.getName());
         System.out.println("findMember = " + findMember.getName());
